ab3a55834697838c26593760b4568ce39cd845c8
danthom1704 <freddrickmacguffin@gmail.com>
2021-07-05 22:20:46 -0400

    Adding Engine Functions

--- Game-Engine.py
+++ Game-Engine.py
@@ -1,38 +1,32 @@
 import GameBoard as board
+import numpy as np
 
-game_board = board.TicTacToe()
 
+class GameEngine:
+    def __init__(self):
+        game_board = board
 
-if game_board.check_valid_input('X', 0, 1):
-    game_board.set_grid_square('X', 0, 1)
-else:
-    print("Invalid")
-game_board.print_grid()
+    def display(self):
+        game_board.print_grid()
 
-if game_board.check_valid_input('X', 0, 0):
-    game_board.set_grid_square('X', 0, 0)
-else:
-    print("Invalid")
+    def update_square(self, player_symbol, x_coordinate, y_coordinate):
+        if game_board.check_valid_input(player_symbol,x_coordinate,y_coordinate):
+            game_board.set_grid_square(player_symbol,x_coordinate,y_coordinate)
+            return "Success"
+        else:
+            return "Failure"
 
-game_board.reset_board()
+    def ai_selection(self, selection_weights):
+        resized_selection_weights = np.reshape(selection_weights, (-1, 3))
+        for y in range(3):
+            for x in range(3):
+                if not game_board.check_valid_input("X", x, y):
+                    resized_selection_weights[x, y] = 0
 
-if game_board.check_valid_input('X', 0, 0):
-    game_board.set_grid_square('X', 0, 0)
-else:
-    print("Invalid")
-game_board.print_grid()
+        highest_weight = np.where(resized_selection_weights == np.amax(resized_selection_weights))
+        print('Tuple of arrays returned : ', highest_weight)
+        print('List of coordinates of maximum value in Numpy array : ')
+        # zip the 2 arrays to get the exact coordinates
+        selected_move = list(zip(highest_weight[0], highest_weight[1]))
+        return highest_weight[0], highest_weight[1]
 
-if game_board.check_valid_input('X', 0, 1):
-    game_board.set_grid_square('X', 0, 1)
-else:
-    print("Invalid")
-
-print(game_board.is_winner("X"))
-
-game_board.set_grid_square('X', 1, 1)
-game_board.set_grid_square('X', 2, 2)
-
-game_board.print_grid()
-print(game_board.is_winner("X"))
-
-print("Get Test")
