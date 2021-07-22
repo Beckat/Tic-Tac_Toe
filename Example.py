c97ade6d064cdd5c3590ddad7f93fd32b0da7f20
danthom1704 <freddrickmacguffin@gmail.com>
2021-07-21 22:44:26 -0400

    Adding a few comments to Example and setting names to test1, test2, and test3 instead of inaccurate size names

--- Example.py
+++ Example.py
@@ -2,6 +2,7 @@ import GameEngine as TicTacToe
 import Neural_Network as Neural_Network
 import torch
 
+# Runs on GPU if possible
 if torch.cuda.is_available():
     device = torch.device("cuda:0")  # you can continue going on here, like cuda:1 cuda:2....etc.
     print("Running on the GPU")
@@ -11,34 +12,38 @@ else:
 
 env = TicTacToe.GameEngine()
 
+# Initalize the neural nets and load presaved weights
 test_1_hidden_size = Neural_Network.Network(env, 16, 504)
-test_125_hidden_size = Neural_Network.Network(env, 16, 504)
-test_50_hidden_size = Neural_Network.Network(env, 50)
+test_2_hidden_size = Neural_Network.Network(env, 16, 504)
+test_3_hidden_size = Neural_Network.Network(env, 50)
 test_1_hidden_size.load_state_dict(torch.load("/home/danthom1704/PycharmProjects/Tic-Tac_toe/opp_nn_tic_tac_toe_50_v4"))
-test_125_hidden_size.load_state_dict(torch.load("/home/danthom1704/PycharmProjects/Tic-Tac_toe/nn_tic_tac_toe_50_v4"))
-test_50_hidden_size.load_state_dict(torch.load("/home/danthom1704/PycharmProjects/Tic-Tac_toe/nn_tic_tac_toe_target_50_expanded"))
+test_2_hidden_size.load_state_dict(torch.load("/home/danthom1704/PycharmProjects/Tic-Tac_toe/nn_tic_tac_toe_50_v4"))
+test_3_hidden_size.load_state_dict(torch.load("/home/danthom1704/PycharmProjects/Tic-Tac_toe/nn_tic_tac_toe_target_50_expanded"))
 test_1_hidden_size.to(device)
-test_125_hidden_size.to(device)
-test_50_hidden_size.to(device)
+test_2_hidden_size.to(device)
+test_3_hidden_size.to(device)
 
 obs = env.reset()
 
+# Sets each score to 0
 test_1_hidden_size_wins_going_first = 0
-test_125_hidden_size_wins_going_second = 0
-test_50_hidden_size_wins_going_second = 0
+test_2_hidden_size_wins_going_second = 0
+test_3_hidden_size_wins_going_second = 0
 ties_1_going_first = 0
 test_1_hidden_size_wins_going_second = 0
-test_125_hidden_size_wins_going_first = 0
-test_50_hidden_size_wins_going_first = 0
-ties_125_going_first = 0
-ties_50_going_first = 0
-ties_50_going_first_round_2 = 0
-test_50_hidden_size_wins_going_first_round_2 = 0
-test_50_hidden_size_wins_going_second_round_2 = 0
-test_125_hidden_size_wins_going_second_round_2 = 0
-test_125_hidden_size_wins_going_first_round_2 = 0
-ties_125_going_first_round_2 = 0
+test_2_hidden_size_wins_going_first = 0
+test_3_hidden_size_wins_going_first = 0
+ties_2_going_first = 0
+ties_3_going_first = 0
+ties_3_going_first_round_2 = 0
+test_3_hidden_size_wins_going_first_round_2 = 0
+test_3_hidden_size_wins_going_second_round_2 = 0
+test_2_hidden_size_wins_going_second_round_2 = 0
+test_2_hidden_size_wins_going_first_round_2 = 0
+ties_2_going_first_round_2 = 0
 
+
+# Compares Test 1 and Test 2 with test 1 going first across all 9 starting squares
 for x in range(1, 10):
     obs = env.reset()
     env.update_square("X", x)
@@ -47,11 +52,11 @@ for x in range(1, 10):
         print("")
 
         obs = env.get_ai_state('O')
-        env.update_square('O', test_125_hidden_size.act(obs, env, device) + 1)
+        env.update_square('O', test_2_hidden_size.act(obs, env, device) + 1)
         env.game_board.print_grid()
         print("")
         if env.is_winner("O"):
-            test_125_hidden_size_wins_going_second = test_125_hidden_size_wins_going_second + 1
+            test_2_hidden_size_wins_going_second = test_2_hidden_size_wins_going_second + 1
             break
 
         obs = env.get_ai_state('X')
@@ -69,6 +74,8 @@ for x in range(1, 10):
 
 print("")
 print("")
+
+# Compares Test 1 and Test 2 with test 2 going first across all 9 starting squares
 for x in range(1, 10):
     obs = env.reset()
     env.update_square("X", x)
@@ -85,16 +92,16 @@ for x in range(1, 10):
             break
 
         obs = env.get_ai_state('X')
-        env.update_square('X', test_125_hidden_size.act(obs, env, device) + 1)
+        env.update_square('X', test_2_hidden_size.act(obs, env, device) + 1)
         env.game_board.print_grid()
         print("")
 
         if env.is_winner("X"):
-            test_125_hidden_size_wins_going_first = test_125_hidden_size_wins_going_first + 1
+            test_2_hidden_size_wins_going_first = test_2_hidden_size_wins_going_first + 1
             break
 
         if len(env.list_of_valid_moves()) == 0:
-            ties_125_going_first = ties_125_going_first + 1
+            ties_2_going_first = ties_2_going_first + 1
             break
 
 
@@ -102,6 +109,7 @@ ties_1_going_first_round_2 = 0
 test_1_hidden_size_wins_going_first_round_2 = 0
 test_1_hidden_size_wins_going_second_round_2 = 0
 
+# Compares Test 1 and Test 3 with test 1 going first across all 9 starting squares
 for x in range(1, 10):
     obs = env.reset()
     env.update_square("X", x)
@@ -110,11 +118,11 @@ for x in range(1, 10):
         print("")
 
         obs = env.get_ai_state('O')
-        env.update_square('O', test_50_hidden_size.act(obs, env, device) + 1)
+        env.update_square('O', test_3_hidden_size.act(obs, env, device) + 1)
         env.game_board.print_grid()
         print("")
         if env.is_winner("O"):
-            test_50_hidden_size_wins_going_second = test_50_hidden_size_wins_going_second + 1
+            test_3_hidden_size_wins_going_second = test_3_hidden_size_wins_going_second + 1
             break
 
         obs = env.get_ai_state('X')
@@ -132,6 +140,8 @@ for x in range(1, 10):
 
 print("")
 print("")
+
+# Compares Test 1 and Test 3 with test 3 going first across all 9 starting squares
 for x in range(1, 10):
     obs = env.reset()
     env.update_square("X", x)
@@ -148,20 +158,19 @@ for x in range(1, 10):
             break
 
         obs = env.get_ai_state('X')
-        env.update_square('X', test_50_hidden_size.act(obs, env, device) + 1)
+        env.update_square('X', test_3_hidden_size.act(obs, env, device) + 1)
         env.game_board.print_grid()
         print("")
 
         if env.is_winner("X"):
-            test_50_hidden_size_wins_going_first = test_50_hidden_size_wins_going_first + 1
+            test_3_hidden_size_wins_going_first = test_3_hidden_size_wins_going_first + 1
             break
 
         if len(env.list_of_valid_moves()) == 0:
-            ties_50_going_first = ties_50_going_first + 1
+            ties_3_going_first = ties_3_going_first + 1
             break
 
-
-
+# Compares Test 2 and Test 3 with test 2 going first across all 9 starting squares
 for x in range(1, 10):
     obs = env.reset()
     env.update_square("X", x)
@@ -170,28 +179,30 @@ for x in range(1, 10):
         print("")
 
         obs = env.get_ai_state('O')
-        env.update_square('O', test_50_hidden_size.act(obs, env, device) + 1)
+        env.update_square('O', test_3_hidden_size.act(obs, env, device) + 1)
         env.game_board.print_grid()
         print("")
         if env.is_winner("O"):
-            test_50_hidden_size_wins_going_second_round_2 = test_50_hidden_size_wins_going_second_round_2 + 1
+            test_3_hidden_size_wins_going_second_round_2 = test_3_hidden_size_wins_going_second_round_2 + 1
             break
 
         obs = env.get_ai_state('X')
-        env.update_square('X', test_125_hidden_size.act(obs, env, device) + 1)
+        env.update_square('X', test_2_hidden_size.act(obs, env, device) + 1)
         env.game_board.print_grid()
         print("")
 
         if env.is_winner("X"):
-            test_125_hidden_size_wins_going_first_round_2 = test_125_hidden_size_wins_going_first_round_2 + 1
+            test_2_hidden_size_wins_going_first_round_2 = test_2_hidden_size_wins_going_first_round_2 + 1
             break
 
         if len(env.list_of_valid_moves()) == 0:
-            ties_125_going_first_round_2 = ties_125_going_first_round_2 + 1
+            ties_2_going_first_round_2 = ties_2_going_first_round_2 + 1
             break
 
 print("")
 print("")
+
+# Compares Test 2 and Test 3 with test 3 going first across all 9 starting squares
 for x in range(1, 10):
     obs = env.reset()
     env.update_square("X", x)
@@ -200,59 +211,59 @@ for x in range(1, 10):
         print("")
 
         obs = env.get_ai_state('O')
-        env.update_square('O', test_125_hidden_size.act(obs, env, device) + 1)
+        env.update_square('O', test_2_hidden_size.act(obs, env, device) + 1)
         env.game_board.print_grid()
         print("")
         if env.is_winner("O"):
-            test_125_hidden_size_wins_going_second_round_2 = test_125_hidden_size_wins_going_second_round_2 + 1
+            test_2_hidden_size_wins_going_second_round_2 = test_2_hidden_size_wins_going_second_round_2 + 1
             break
 
         obs = env.get_ai_state('X')
-        env.update_square('X', test_50_hidden_size.act(obs, env, device) + 1)
+        env.update_square('X', test_3_hidden_size.act(obs, env, device) + 1)
         env.game_board.print_grid()
         print("")
 
         if env.is_winner("X"):
-            test_50_hidden_size_wins_going_first_round_2 = test_50_hidden_size_wins_going_first_round_2 + 1
+            test_3_hidden_size_wins_going_first_round_2 = test_3_hidden_size_wins_going_first_round_2 + 1
             break
 
         if len(env.list_of_valid_moves()) == 0:
-            ties_50_going_first_round_2 = ties_50_going_first_round_2 + 1
+            ties_3_going_first_round_2 = ties_3_going_first_round_2 + 1
             break
 
 
-print("1 Wins Going First vs 125: ", test_1_hidden_size_wins_going_first)
-print("125 Wins Going Second: ", test_125_hidden_size_wins_going_second)
-print("Ties 1 Going First vs 125: ", ties_1_going_first)
+print("1 Wins Going First vs 2: ", test_1_hidden_size_wins_going_first)
+print("2 Wins Going Second: ", test_2_hidden_size_wins_going_second)
+print("Ties 1 Going First vs 2: ", ties_1_going_first)
 
 print("")
 
-print("1 Wins Going Second vs 125: ", test_1_hidden_size_wins_going_second)
-print("125 Wins Going First: ", test_125_hidden_size_wins_going_first)
-print("Ties 125 Going First: ", ties_125_going_first)
-
-print("")
-print("")
-
-print("1 Wins Going First vs 50: ", test_1_hidden_size_wins_going_first_round_2)
-print("50 Wins Going Second: ", test_50_hidden_size_wins_going_second)
-print("Ties 50 Going First: ", ties_1_going_first_round_2)
-
-print("")
-
-print("1 Wins Going Second: vs 50: ", test_1_hidden_size_wins_going_second_round_2)
-print("50 Wins Going First: ", test_50_hidden_size_wins_going_first)
-print("Ties 50 Going First: ", ties_50_going_first)
+print("1 Wins Going Second vs 2: ", test_1_hidden_size_wins_going_second)
+print("@ Wins Going First: ", test_2_hidden_size_wins_going_first)
+print("Ties 2 Going First: ", ties_2_going_first)
 
 print("")
 print("")
 
-print("125 Wins Going First vs 50: ", test_125_hidden_size_wins_going_first_round_2)
-print("50 Wins Going Second Round 2: ", test_50_hidden_size_wins_going_second_round_2)
-print("Ties 125 Going First Round 2: ", ties_125_going_first_round_2)
+print("1 Wins Going First vs 3: ", test_1_hidden_size_wins_going_first_round_2)
+print("3 Wins Going Second: ", test_3_hidden_size_wins_going_second)
+print("Ties 3 Going First: ", ties_1_going_first_round_2)
 
 print("")
 
-print("125 Wins Going Second: vs 50: ", test_125_hidden_size_wins_going_second_round_2)
-print("50 Wins Going First: Round 2 ", test_50_hidden_size_wins_going_first_round_2)
-print("Ties 50 Going First Rond 2: ", ties_50_going_first_round_2)
+print("1 Wins Going Second: vs 3: ", test_1_hidden_size_wins_going_second_round_2)
+print("3 Wins Going First: ", test_3_hidden_size_wins_going_first)
+print("Ties 3 Going First: ", ties_3_going_first)
+
+print("")
+print("")
+
+print("2 Wins Going First vs 3: ", test_2_hidden_size_wins_going_first_round_2)
+print("3 Wins Going Second Round 2: ", test_3_hidden_size_wins_going_second_round_2)
+print("Ties 2 Going First Round 2: ", ties_2_going_first_round_2)
+
+print("")
+
+print("2 Wins Going Second: vs 3: ", test_2_hidden_size_wins_going_second_round_2)
+print("3 Wins Going First: Round 2 ", test_3_hidden_size_wins_going_first_round_2)
+print("Ties 3 Going First Round 2: ", ties_3_going_first_round_2)
